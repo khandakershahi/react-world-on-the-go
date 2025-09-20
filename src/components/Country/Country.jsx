5d3aa01c549e6b66c48aae71a2ad7a21e29a602c
khandakershahi <khandakershahi@gmail.com>
2025-09-20 12:38:30 +0600

    style add, 3 col and conditional display

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
+import './Country.css';
 
 const Country = ({ country }) => {
-    // console.log(country.population.population)
+
+    const handleVisited = () => {
+        console.log('Button clicked');
+    }
     return (
-        <div>
+        <div className='country'>
             <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
             <h3>Name: {country.name.common}</h3>
             <p>Population: {country.population.population}</p>
             <p>Capital: {country.capital.capital}</p>
+            <p>Area: {country.area.area} {country.area.area > 30000 ? " Big Country" : " Small Country"}</p>
+            <button onClick={handleVisited}>Not Visited</button>
         </div>
     );
 };
 
 export default Country;
+
+
+/**
+ * 1. inline css (style object)
+ */
