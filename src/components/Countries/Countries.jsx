90daec8d0968609bb409871d8cb1c33526765047
khandakershahi <khandakershahi@gmail.com>
2025-09-21 16:41:37 +0600

    lift up state

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -7,7 +7,9 @@ const Countries = ({ countriesPromise }) => {
     const [visitedCountries, setVisitedCountries] = useState([]);
 
     const handleVisitedCountries = (country) => {
-        console.log('handle visited country clicke', country)
+        // console.log('handle visited country click', country);
+        const newVisitedCountries = [...visitedCountries, country];
+        setVisitedCountries(newVisitedCountries);
     }
 
     const countriesData = use(countriesPromise);
@@ -18,7 +20,14 @@ const Countries = ({ countriesPromise }) => {
     return (
         <div>
             <h1>In the countries: {countries.length}</h1>
-            <h3>Total Country Visited: </h3>
+            <h3>Total Country Visited: {visitedCountries.length} </h3>
+            <ol>
+                {
+                    visitedCountries.map(country => <li
+                        key={country.cca3.cca3}
+                    >{country.name.common}</li>)
+                }
+            </ol>
             <div className='countries'>
                 {
                     countries.map(country => <Country
